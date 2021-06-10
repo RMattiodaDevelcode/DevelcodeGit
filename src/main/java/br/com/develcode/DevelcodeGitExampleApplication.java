4b56f8171486907548ed46a726b71fed97488735
RMattiodaDevelcode <rodrigo.mattioda@develcode.com.br>
2021-06-10 13:44:53 -0300

    First commit

---
+++ src/main/java/br/com/develcode/DevelcodeGitExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.develcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelcodeGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelcodeGitExampleApplication.class, args);
+	}
+
+}
