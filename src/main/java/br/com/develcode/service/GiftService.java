18ba8fd5a8ba2e7089418028f47d59c4336088f5
RMattioda <rodrigo-mattioda@hotmail.com>
2021-06-29 23:09:19 -0300

    adding new set of rules

--- src/main/java/br/com/develcode/service/GiftService.java
+++ src/main/java/br/com/develcode/service/GiftService.java
@@ -2,10 +2,13 @@ package br.com.develcode.service;
 
 import br.com.develcode.common.model.GiftModel;
 import br.com.develcode.common.presenter.GiftPresenter;
+import br.com.develcode.common.type.SectionType;
 import br.com.develcode.data.dao.GiftDao;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class GiftService {
@@ -13,12 +16,20 @@ public class GiftService {
     @Autowired
     private GiftDao giftDao;
 
-    public GiftPresenter findRandomGift(){
+    public GiftPresenter findRandomGift() {
         GiftPresenter presenter = new GiftPresenter();
 
         GiftModel giftModel = giftDao.findRandomGift();
         BeanUtils.copyProperties(giftModel, presenter);
 
+        if (SectionType.TOYS.getName().equalsIgnoreCase(presenter.getSection())) {
+            presenter.setPrice(addDiscount(presenter.getPrice(), BigDecimal.valueOf(0.25)));
+        }
+
         return presenter;
+    }
+
+    private BigDecimal addDiscount(BigDecimal price, BigDecimal percentMultiplier) {
+        return price.subtract(price.multiply(percentMultiplier));
     }
 }
